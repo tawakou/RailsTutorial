96d8301d51856f1027b618ed7cc1506205062fc9
たわこう <koutawamr@gmail.com>
2017-09-13 13:49:40 +0900

    初期

--- three/config/routes.rb
+++ three/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get '/home', to:'hp#home'
   post '/login',to:'user#login'
   get '/new',to:'user#new'
-
+  get '/aaa',to:'hp#aaa'
+  get '/info',to:'hp#info'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- zenbu/app/controllers/users_controller.rb
+++ zenbu/app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
-  
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  def create
+    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
 end
